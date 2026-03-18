6278528f8d7796753d74c500bfb9208c30a3242d
Joao Victor <joao.curto.almeida@example.com>
2026-03-18 10:47:54 -0300

    18-03-26 Criação Registro component e rotas.
    ajuste angular.jason para o uso da pasta assets.
    criação do componente cadastro.
    - foi criado no componente a tela de cadastro e foi implementado uma rota para login.(registro->login)
    .
    criado uma rota para o componente login.(login->registro).
    ajuste de rotas no arquivo app.routes.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CadastroComponent } from "./cadastro.component/cadastro.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CadastroComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class AppComponent { 
-  
+export class AppComponent {
+
 }
---
+++ src/app/cadastro.component/cadastro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [RouterModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css',
+})
+export class CadastroComponent {}
